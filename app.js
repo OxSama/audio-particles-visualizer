ef1a138caf3f35a12c0e41c21b6d82a9b650e480
OxSama <mhmdtageldin@gmail.com>
2023-07-24 16:35:54 +0400

    restore previous particles

--- app.js
+++ app.js
@@ -1,64 +1,113 @@
 particlesJS('particles-js', {
-
-    fpsLimit: 120,
-
-    interactivity: {
-        detect_on: 'canvas',
-        events: {
-            onClick: {
-                enable: true,
-                mode: "push",
-            },
-            resize: true,
+    "particles": {
+      "number": {
+        "value": 80,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
         },
-        modes: {
-            grab: { distance: 400, line_linked: { opacity: 1 } },
-            bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-            repulse: { distance: 200, duration: 0.4 },
-            push: { particles_nb: 4 },
-            remove: { particles_nb: 2 },
+        "polygon": {
+          "nb_sides": 5
         },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 10,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 80,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 300,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 2
+      },
+      "move": {
+        "enable": true,
+        "speed": 12,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
     },
-    particles: {
-        color: {
-            value: ["#000000", "#1a365d", "#881c24", "#b7b7b7"],
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode": "repulse"
         },
-        links: {
-            color: "#ffffff",
-            distance: 200,
-            enable: true,
-            opacity: 0.1,
-            width: 1,
+        "onclick": {
+          "enable": true,
+          "mode": "push"
         },
-        move: {
-            direction: "none",
-            enable: true,
-            outModes: {
-                default: "bounce-horizontal",
-            },
-            random: false,
-            speed: 5,
-            straight: false,
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 800,
+          "line_linked": {
+            "opacity": 1
+          }
         },
-        number: {
-            density: {
-                enable: true,
-                area: 1000,
-            },
-            value: 80,
+        "bubble": {
+          "distance": 800,
+          "size": 80,
+          "duration": 2,
+          "opacity": 0.8,
+          "speed": 3
         },
-        opacity: {
-            value: 0.5,
+        "repulse": {
+          "distance": 400,
+          "duration": 0.4
         },
-        shape: {
-            type: ["square", "circle", "triangle"],
+        "push": {
+          "particles_nb": 4
         },
-        size: {
-            value: { min: 1, max: 5 },
-        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
     },
-    retina_detect: true,
-});
+    "retina_detect": true
+  });
 
 var audioContext = new AudioContext();
 var analyser = audioContext.createAnalyser();
